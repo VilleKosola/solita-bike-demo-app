49a19633fcefa9fc59ecbc36ffe84bb9d8a528ec
Ville Kosola <ville.kosola@solita.fi>
2022-11-10 16:04:18 +0200

    add pagination

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Journeys from './components/journeys';
 
 function App() {
+  const [limit, setLimit] = React.useState(100);
+  const [offset, setOffset] = React.useState(0);
+  const [orderby, setOrderBy] = React.useState('');
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div className='flex justify-evenly'>
+        <div className='font-bold rounded-full bg-black text-white w-7 h-7 flex items-center justify-center cursor-pointer' onClick={() => setOffset(offset-limit)}> <span>{'<'}</span> </div>
+        <div>
+          <select value={limit} onChange={(e) => {setLimit(Number(e.target.value)); setOffset(Math.floor(offset/Number(e.target.value)))}}>
+            <option value={10}>10</option>
+            <option value={50}>50</option>
+            <option value={100}>100</option>
+            <option value={1000}>1000</option>
+          </select>
+        </div>
+        <div className='font-bold rounded-full bg-black text-white w-7 h-7 flex items-center justify-center cursor-pointer' onClick={() => setOffset(offset+limit)}> {'>'} </div>
+      </div>
+      <Journeys offset={offset} limit={limit} />
     </div>
   );
 }
--- frontend/src/components/journeys.tsx
+++ frontend/src/components/journeys.tsx
@@ -12,25 +12,39 @@ type Journey = {
   duration: number
 }
 
-function Journeys() {
+function Journeys(props: {offset: number, limit: number}) {
   const [journeys, setJourneys] = React.useState([]);
 
   React.useEffect(() => {
+    console.log('fetching from:', props.offset)
     axios
-      .get("http://localhost:3002/journey/100/0")
+      .get(`http://localhost:3002/journey/${props.limit}/${props.offset}`)
       .then((response) => setJourneys(response.data));
-  }, []);
+  }, [props]);
 
   return (
-    <ul className="journeys">
-      {journeys.map((journey: Journey, i: number) => (
-        <li className="grid grid-cols-3 p-2 border-b" key={journey.id}>
-          <h4 className="text-left"> {i+1}. {journey.departure_station_name} - {journey.return_station_name}</h4>
-          <p className="">{dayjs(journey.departuredate).format('DD.MM.YYYY')} - {dayjs(journey.returndate).format('DD.MM.YYYY')}</p>
-          <p className="text-right">Distance: {journey.distance / 1000}km Duration: {Math.floor(journey.duration/60) + 'min ' + journey.duration%60}s</p>
+    <div>
+      <ul className="journeys">
+        <li className="grid grid-cols-6 p-2 border-b" key={'header'}>
+          <h4 className="text-left"> #. </h4>
+          <h4 className="text-left col-span-2"> Stations </h4>
+          <p className="text-left"> Dates </p>
+          <p className="text-left">Distance</p>
+          <p className="text-left">Duration</p>
         </li>
-      ))}
-    </ul>
+      </ul>
+      <ul className="journeys">
+        {journeys.map((journey: Journey, i: number) => (
+          <li className="grid grid-cols-6 p-2 border-b" key={journey.id}>
+            <h4 className="text-left"> {i+1+props.offset}. </h4>
+            <h4 className="text-left col-span-2"> {journey.departure_station_name} - {journey.return_station_name}</h4>
+            <p className="text-left">{dayjs(journey.departuredate).format('DD.MM.YYYY')} - {dayjs(journey.returndate).format('DD.MM.YYYY')}</p>
+            <p className="text-left">{journey.distance / 1000}km</p>
+            <p className="text-left">{Math.floor(journey.duration/60) + 'min ' + journey.duration%60}s</p>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
