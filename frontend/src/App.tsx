1712351006b587f51a207a9c721ead810715057a
Ville Kosola <ville.kosola@solita.fi>
2022-11-11 12:04:23 +0200

    move pagination to its own component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,41 +1,23 @@
-import React from 'react';
 import './App.css';
 import Journeys from './pages/journeys';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navigation from './pages/navigation';
 import Stations from './pages/stations';
 
 function App() {
-  const [limit, setLimit] = React.useState(100);
-  const [offset, setOffset] = React.useState(0);
-  const [orderby, setOrderBy] = React.useState('');
-
   return (
-    <BrowserRouter>      
+    <BrowserRouter>
       <header className="App-header h-10 flex justify-center">
         Helsinki city bike journeys app
       </header>
-      <div className='flex justify-evenly p-3'>
-        {offset > 0 ?     
-          <div className='font-bold rounded-full bg-black text-white w-7 h-7 flex items-center justify-center cursor-pointer' onClick={() => setOffset(offset-limit)}> <span>{'<'}</span> </div>
-          :
-          <div></div>
-        }
-        <div>
-          <select value={limit} onChange={(e) => {setLimit(Number(e.target.value)); setOffset(Math.floor(offset/Number(e.target.value)))}}>
-            <option value={10}>10</option>
-            <option value={50}>50</option>
-            <option value={100}>100</option>
-            <option value={1000}>1000</option>
-          </select>
-        </div>
-        <div className='font-bold rounded-full bg-black text-white w-7 h-7 flex items-center justify-center cursor-pointer' onClick={() => setOffset(offset+limit)}> {'>'} </div>
-      </div>
+
       <Routes>
-      {/* offsetChange={(value: number) => setOffset(value)} limitChange={(value: number) => setLimit(value)} orderbyChange={(value: string) => setOrderBy(value)} */}
         <Route path="*" element={<Navigation />}>
-          <Route path="journeys" element={<Journeys offset={offset} limit={limit}/>} />
-          <Route path="stations" element={<Stations  offset={offset} limit={limit}/>} />
+          <Route path="journeys" element={<Journeys />} />
+          <Route
+            path="stations"
+            element={<Stations/>}
+          />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ frontend/src/components/pagination.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+
+interface paginationProps {
+  offsetChange: any;
+  limitChange: any;
+  limit: number;
+  offset: number;
+}
+
+const Pagination = (props: paginationProps) => {
+  const [limit, setLimit] = React.useState(props.limit);
+  const [offset, setOffset] = React.useState(props.offset);
+
+  const limitselection = (value: number) => {
+    props.limitChange(value);
+    setLimit(value);
+    props.offsetChange(0);
+    setOffset(0);
+  }
+
+  return (
+    <div className="flex justify-between p-3">
+        <div></div>
+      {offset > 0 ? (
+        <div
+          className="font-bold rounded-full bg-black text-white w-7 h-7 flex items-center justify-center cursor-pointer"
+          onClick={() => {props.offsetChange(offset - limit); setOffset(offset - limit)}}
+        >
+          {' '}
+          <span>{'<'}</span>{' '}
+        </div>
+      ) : (
+        <div></div>
+      )}
+
+      <div className='text-center'>
+        {offset} - {offset + limit}
+      </div>
+
+      <div
+        className="font-bold rounded-full bg-black text-white w-7 h-7 flex items-center justify-center cursor-pointer"
+        onClick={() => {props.offsetChange(offset + limit); setOffset(offset + limit)}}
+      >
+        {' '}
+        {'>'}{' '}
+      </div>
+      <div>
+        <select
+          value={limit}
+          onChange={(e) => {
+            limitselection(Number(e.target.value))
+          }}
+        >
+          <option value={10}>10</option>
+          <option value={50}>50</option>
+          <option value={100}>100</option>
+          <option value={1000}>1000</option>
+          <option value={10000}>10000</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default Pagination;
