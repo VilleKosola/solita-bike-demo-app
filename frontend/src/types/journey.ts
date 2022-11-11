0e83b0041af6936b8180976e80390208859b5fed
Ville Kosola <ville.kosola@solita.fi>
2022-11-11 12:02:49 +0200

    add journey and station types

---
+++ frontend/src/types/journey.ts
@@ -0,0 +1,9 @@
+export interface Journey {
+  id: string;
+  departuredate: string;
+  returndate: string;
+  return_station_name: string;
+  departure_station_name: string;
+  distance: number;
+  duration: number;
+}
---
+++ frontend/src/types/station.ts
@@ -0,0 +1,11 @@
+export interface Station {
+  id: number;
+  fid: number;
+  nimi: string;
+  osoite: string;
+  city: string;
+  operator: string;
+  capasity: string;
+  x_coordinate: number;
+  y_coordinate: number;
+}
