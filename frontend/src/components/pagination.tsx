69ce52d66e23aa0b109b09ed07eaf170f2ff33f2
Ville Kosola <ville.kosola@solita.fi>
2022-11-11 15:47:29 +0200

    fix lint warning

--- frontend/src/components/pagination.tsx
+++ frontend/src/components/pagination.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 interface paginationProps {
-  offsetChange: any;
-  limitChange: any;
+  offsetChange: {(value: number): void};
+  limitChange: {(value: number): void};
   limit: number;
   offset: number;
 }
