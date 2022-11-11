695a52e3f124e4a7d25b0fddad63d786780f0ad7
Ville Kosola <ville.kosola@solita.fi>
2022-11-11 12:03:40 +0200

    format navigation

--- frontend/src/pages/navigation.tsx
+++ frontend/src/pages/navigation.tsx
@@ -1,26 +1,34 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
-import NavItem from "../components/navitem";
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import NavItem from '../components/navitem';
 
 const Navigation = () => {
-    const [path, setPath] = React.useState('/journeys');
+  const [path, setPath] = React.useState('/journeys');
 
   return (
     <>
       <nav>
         <ul className="flex justify-center p-3 cursor-pointer">
-            <li onClick={() => setPath('/journeys')}>
-                <NavItem name={"Journeys"} path={"/journeys"} active={path === '/journeys'} />
-            </li>
-            <li onClick={() => setPath('/stations')}>
-                <NavItem name={"Stations"} path={"/stations"} active={path === '/stations'} />
-            </li>
+          <li onClick={() => setPath('/journeys')}>
+            <NavItem
+              name={'Journeys'}
+              path={'/journeys'}
+              active={path === '/journeys'}
+            />
+          </li>
+          <li onClick={() => setPath('/stations')}>
+            <NavItem
+              name={'Stations'}
+              path={'/stations'}
+              active={path === '/stations'}
+            />
+          </li>
         </ul>
       </nav>
 
       <Outlet />
     </>
-  )
+  );
 };
 
 export default Navigation;
