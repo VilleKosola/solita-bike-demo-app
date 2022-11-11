b748a8980bc1b696f3a4a444522cdcdf93017a83
Ville Kosola <ville.kosola@solita.fi>
2022-11-11 12:04:56 +0200

    add ordering to journeys and stations pages

--- frontend/src/pages/journeys.tsx
+++ frontend/src/pages/journeys.tsx
@@ -1,55 +1,81 @@
-import React from "react";
-import axios from "axios";
-import dayjs from "dayjs";
+import React from 'react';
+import axios from 'axios';
+import dayjs from 'dayjs';
+import { Journey } from '../types/journey';
+import Pagination from '../components/pagination';
 
-type Journey = {
-  id: string,
-  departuredate: string,
-  returndate: string,
-  return_station_name: string,
-  departure_station_name: string,
-  distance: number,
-  duration: number
-}
-
-function Journeys(props: {offset: number, limit: number}) {
+const Journeys = () => {
   const [journeys, setJourneys] = React.useState([]);
+  const [orderby, setOrderBy] = React.useState('');
+  const [limit, setLimit] = React.useState(100);
+  const [offset, setOffset] = React.useState(0);
+  const [ordering, setOrdering] = React.useState('ASC');
+
+  const toggleOrdering = () => {
+    setOrdering(ordering === 'ASC' ? 'DESC' : 'ASC');
+  }
 
   React.useEffect(() => {
-    console.log('fetching from:', props.offset)
+    console.log('fetching from:', offset);
     axios
-      .get(`http://localhost:3002/journey/${props.limit}/${props.offset}`)
+      .get(`http://localhost:3002/journey`, {params: {limit, offset, orderby, ordering}})
       .then((response) => setJourneys(response.data));
-  }, [props]);
+  }, [orderby, limit, offset, ordering]);
 
   return (
     <div>
+      <Pagination
+        limit={limit}
+        offset={offset}
+        offsetChange={(value: number) => setOffset(value)}
+        limitChange={(value: number) => setLimit(value)}
+      />
       <ul className="journeys bg-slate-300 font-bold">
         <li className="grid grid-cols-9 p-2 border-b" key={'header'}>
           <p className="text-left"> #. </p>
-          <p className="text-left col-span-2"> Start station </p>
-          <p className="text-left col-span-2"> End station </p>
-          <p className="text-left"> Start date </p>
-          <p className="text-left"> End date </p>
-          <p className="text-left">Distance</p>
-          <p className="text-left">Duration</p>
+          <p className="text-left col-span-2 cursor-pointer" onClick={() => {setOrderBy('departure_station_name'); toggleOrdering()}}> Start station </p>
+          <p className="text-left col-span-2 cursor-pointer" onClick={() => {setOrderBy('return_station_name'); toggleOrdering()}}> End station </p>
+          <p className="text-left cursor-pointer" onClick={() => {setOrderBy('departuredate'); toggleOrdering()}}> Start date </p>
+          <p className="text-left cursor-pointer" onClick={() => {setOrderBy('returndate'); toggleOrdering()}}> End date </p>
+          <p className="text-left cursor-pointer" onClick={() => {setOrderBy('distance'); toggleOrdering()}}>Distance</p>
+          <p className="text-left cursor-pointer" onClick={() => {setOrderBy('duration'); toggleOrdering()}}>Duration</p>
         </li>
       </ul>
       <ul className="journeys">
         {journeys.map((journey: Journey, i: number) => (
           <li className="grid grid-cols-9 p-2 border-b" key={journey.id}>
-            <p className="text-left"> {i+1+props.offset}. </p>
-            <p className="text-left col-span-2"> {journey.departure_station_name}</p>
-            <p className="text-left col-span-2"> {journey.return_station_name}</p>
-            <p className="text-left">{dayjs(journey.departuredate).format('DD.MM.YYYY')}</p>
-            <p className="text-left">{dayjs(journey.returndate).format('DD.MM.YYYY')}</p>
+            <p className="text-left"> {i + 1 + offset}. </p>
+            <p className="text-left col-span-2">
+              {' '}
+              {journey.departure_station_name}
+            </p>
+            <p className="text-left col-span-2">
+              {' '}
+              {journey.return_station_name}
+            </p>
+            <p className="text-left">
+              {dayjs(journey.departuredate).format('DD.MM.YYYY')}
+            </p>
+            <p className="text-left">
+              {dayjs(journey.returndate).format('DD.MM.YYYY')}
+            </p>
             <p className="text-left">{journey.distance / 1000}km</p>
-            <p className="text-left">{Math.floor(journey.duration/60) + 'min ' + journey.duration%60}s</p>
+            <p className="text-left">
+              {Math.floor(journey.duration / (60*60*24)) +
+                'vrk ' +
+                Math.floor(journey.duration % (60*60*24)/(60*60)) +
+                'h ' +
+                (Math.floor(journey.duration % (60*60)/60)) +
+                'min ' +
+                (journey.duration % 60)
+                }
+              s
+            </p>
           </li>
         ))}
       </ul>
     </div>
   );
-}
+};
 
 export default Journeys;
--- frontend/src/pages/stations.tsx
+++ frontend/src/pages/stations.tsx
@@ -1,46 +1,48 @@
-import React from "react";
-import axios from "axios";
-import dayjs from "dayjs";
+import React from 'react';
+import axios from 'axios';
+import { Station } from '../types/station';
+import Pagination from '../components/pagination';
 
-type Station = {
-  id: number,
-  fid: number,
-  nimi: string,
-  osoite: string,
-  city: string,
-  operator: string,
-  capasity: string,
-  x_coordinate: number,
-  y_coordinate: number
-}
-
-function Stations(props: {offset: number, limit: number}) {
+const Stations = () => {
   const [stations, setStations] = React.useState([]);
+  const [orderby, setOrderBy] = React.useState('nimi');
+  const [limit, setLimit] = React.useState(100);
+  const [offset, setOffset] = React.useState(0);
+  const [ordering, setOrdering] = React.useState('ASC');
+
+  const toggleOrdering = () => {
+    setOrdering(ordering === 'ASC' ? 'DESC' : 'ASC');
+  }
 
   React.useEffect(() => {
-    console.log('fetching from:', props.offset)
     axios
-      .get(`http://localhost:3002/station/${props.limit}/${props.offset}`)
+      .get(`http://localhost:3002/station`, {params: {limit, offset, orderby, ordering}})
       .then((response) => setStations(response.data));
-  }, [props]);
+  }, [orderby, limit, offset, ordering]);
 
   return (
     <div>
+      <Pagination
+        limit={limit}
+        offset={offset}
+        offsetChange={(value: number) => setOffset(value)}
+        limitChange={(value: number) => setLimit(value)}
+      />
       <ul className="journeys bg-slate-300 font-bold">
         <li className="grid grid-cols-9 p-2 border-b" key={'header'}>
           <p className="text-left"> #. </p>
-          <p className="text-left col-span-2"> Name </p>
-          <p className="text-left col-span-2"> Address </p>
-          <p className="text-left"> City </p>
-          <p className="text-left"> Operator </p>
-          <p className="text-left"> X-coordinate </p>
-          <p className="text-left"> Y-coordinate </p>
+          <p className="text-left col-span-2 cursor-pointer" onClick={() => {setOrderBy('nimi'); toggleOrdering()}}> Name </p>
+          <p className="text-left col-span-2 cursor-pointer" onClick={() => {setOrderBy('osoite'); toggleOrdering()}}> Address </p>
+          <p className="text-left cursor-pointer" onClick={() => {setOrderBy('city'); toggleOrdering()}}> City </p>
+          <p className="text-left cursor-pointer" onClick={() => {setOrderBy('operator'); toggleOrdering()}}> Operator </p>
+          <p className="text-left cursor-pointer" onClick={() => {setOrderBy('x_coordinate'); toggleOrdering()}}> X-coordinate </p>
+          <p className="text-left cursor-pointer" onClick={() => {setOrderBy('y_coordinate'); toggleOrdering()}}> Y-coordinate </p>
         </li>
       </ul>
       <ul className="journeys">
         {stations.map((station: Station, i: number) => (
           <li className="grid grid-cols-9 p-2 border-b" key={station.id}>
-            <p className="text-left"> {i+1+props.offset}. </p>
+            <p className="text-left"> {i + 1 + offset}. </p>
             <p className="text-left col-span-2"> {station.nimi}</p>
             <p className="text-left col-span-2"> {station.osoite}</p>
             <p className="text-left">{station.city}</p>
@@ -52,6 +54,6 @@ function Stations(props: {offset: number, limit: number}) {
       </ul>
     </div>
   );
-}
+};
 
 export default Stations;
