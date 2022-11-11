b3dadf4c0d0030ad164e11079729581f63d8369b
Ville Kosola <ville.kosola@solita.fi>
2022-11-11 12:04:37 +0200

    format navitem

--- frontend/src/components/navitem.tsx
+++ frontend/src/components/navitem.tsx
@@ -1,15 +1,22 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 interface navitemprops {
-    name: string,
-    path: string,
-    active: boolean
+  name: string;
+  path: string;
+  active: boolean;
 }
 
 const NavItem = (props: navitemprops) => {
   return (
-    <Link to={props.path}  className={props.active ? 'p-3 bg-slate-600 text-white' : 'p-3 bg-slate-200'}>{props.name}</Link>
-  )
+    <Link
+      to={props.path}
+      className={
+        props.active ? 'p-3 bg-slate-600 text-white' : 'p-3 bg-slate-200'
+      }
+    >
+      {props.name}
+    </Link>
+  );
 };
 
 export default NavItem;
