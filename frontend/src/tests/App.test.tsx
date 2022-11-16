d5159cdbde29126690ad12d9138c00ed37448a06
Ville Kosola <ville.kosola@solita.fi>
2022-11-16 14:37:05 +0200

    add test to app comp

---
+++ frontend/src/tests/App.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+
+test('Two link buttons', () => {
+  render(<App />);
+  const link1 = screen.getByText('Journeys');
+  const link2 = screen.getByText(/Stations/i);
+  expect(link2).toBeInTheDocument();
+  expect(link1).toBeInTheDocument();
+});
