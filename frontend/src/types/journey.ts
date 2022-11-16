16fe3ae5981a87cbdd9f73d80d2b0ecf60694f97
Ville Kosola <ville.kosola@solita.fi>
2022-11-16 14:33:40 +0200

    add missing type attributes

--- frontend/src/types/journey.ts
+++ frontend/src/types/journey.ts
@@ -1,9 +1,11 @@
 export interface Journey {
-  id: string;
+  id: string | number;
   departuredate: string;
   returndate: string;
   return_station_name: string;
   departure_station_name: string;
+  departure_station: number;
+  return_station: number;
   distance: number;
   duration: number;
 }
--- frontend/src/types/station.ts
+++ frontend/src/types/station.ts
@@ -2,10 +2,14 @@ export interface Station {
   id: number;
   fid: number;
   nimi: string;
+  name: string;
+  namn: string;
   osoite: string;
+  address: string;
   city: string;
+  stad: string;
   operator: string;
-  capasity: string;
+  capasity: string|number;
   x_coordinate: number;
   y_coordinate: number;
 }
