5ca3664d4a1845aa489bca35d8d945bf901ca2c4
Ville Kosola <ville.kosola@solita.fi>
2022-11-16 15:58:32 +0200

    add logic to show map of selected station

--- frontend/src/components/stations-list.tsx
+++ frontend/src/components/stations-list.tsx
@@ -1,19 +1,24 @@
+import React from 'react';
 import { Station } from '../types/station';
 import StationItem from './station-item';
 
 const StationList = (props: { stations: Station[]; offset: number }) => {
-  return (
-    <ul className="stations">
-      {props.stations.map((station: Station, i: number) => (
-        <StationItem
-          data-testid="station-item"
-          key={station.id}
-          station={station}
-          index={i + 1 + props.offset}
-        />
-      ))}
-    </ul>
-  );
+    const [active, setActive] = React.useState('')
+    return (
+        <ul className="stations">
+        {props.stations.map((station: Station, i: number) => (
+        
+            <StationItem
+                data-testid="station-item"
+                key={station.id}
+                station={station}
+                index={i + 1 + props.offset} 
+                active={active == station.id.toString()} 
+                setActive={(id: string) => id == active ? setActive('') : setActive(id)}            
+                />
+        ))}
+        </ul>
+    );
 };
 
 export default StationList;
