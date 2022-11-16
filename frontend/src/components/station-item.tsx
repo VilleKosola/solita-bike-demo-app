27d2416a5e7b2f964da357695b74819a1c9f5712
Ville Kosola <ville.kosola@solita.fi>
2022-11-16 15:58:01 +0200

    add map to station item

--- frontend/src/components/station-item.tsx
+++ frontend/src/components/station-item.tsx
@@ -1,18 +1,25 @@
+import React from 'react';
 import { Station } from '../types/station';
+import LeafletMap from './map';
 
-const StationItem = (props: { station: Station; index: number }) => {
+const StationItem = (props: { station: Station, index: number, active: boolean, setActive: { (value: string): void }}) => {
   const station = props.station;
 
   return (
-    <li className="grid grid-cols-9 p-2 border-b" key={station.fid} data-testid="station-item">
-      <p className="text-left"> {props.index}. </p>
-      <p className="text-left col-span-2"> {station.nimi}</p>
-      <p className="text-left col-span-2"> {station.osoite}</p>
-      <p className="text-left">{station.city}</p>
-      <p className="text-left">{station.operator}</p>
-      <p className="text-left">{station.x_coordinate}</p>
-      <p className="text-left">{station.y_coordinate}</p>
-    </li>
+    <>
+      <li className="grid grid-cols-9 p-2 border-b cursor-pointer hover:bg-gray-200" key={station.fid} data-testid="station-item" onClick={() => props.setActive(station.id.toString())}>
+        <p className="text-left"> {props.index}. </p>
+        <p className="text-left col-span-2"> {station.nimi}</p>
+        <p className="text-left col-span-2"> {station.osoite}</p>
+        <p className="text-left">{station.city}</p>
+        <p className="text-left">{station.operator}</p>
+        <p className="text-left">{station.x_coordinate}</p>
+        <p className="text-left">{station.y_coordinate}</p>
+      </li>
+      { props.active && (
+        <LeafletMap locations={[{x: station.x_coordinate, y: station.y_coordinate, name: station.nimi, id: station.fid}]} />
+      )}
+    </>
   );
 };
 
