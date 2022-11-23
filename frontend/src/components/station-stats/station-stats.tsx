b8264813f42e2e059d9caa1c644cfc4df0c5f15b
Ville Kosola <ville.kosola@solita.fi>
2022-11-23 14:41:18 +0200

    add type

--- frontend/src/components/station-stats/station-stats.tsx
+++ frontend/src/components/station-stats/station-stats.tsx
@@ -3,13 +3,18 @@ import React from 'react';
 import { getStationStatistics } from '../../services/StationService';
 import { StationStatistics } from '../../types/station';
 
-const selections: any[] = [
+interface SelectObject {
+    name: string;
+    from: string;
+    to: string;
+}
+
+const selections: SelectObject[] = [
     {name: 'All', from:'2018-01-01 00:00', to: '2023-01-01 00:00'},
     {name: 'May 2021', from:'2021-05-01 00:00', to: '2021-06-01 00:00'},
     {name: 'June 2021', from:'2021-06-01 00:00', to: '2021-07-01 00:00'},
     {name: 'July 2021', from:'2021-07-01 00:00', to: '2021-08-01 00:00'},
     {name: 'August 2021', from:'2021-08-01 00:00', to: '2021-08-01 00:00'},
-
 ]
 
 const StationStats = (props: {
@@ -19,7 +24,7 @@ const StationStats = (props: {
     const [timeFrame, setTimeFrame] = React.useState('All')
 
     React.useEffect(() => {
-        const selection = selections.find(s => s.name === timeFrame)
+        const selection = selections.find(s => s.name === timeFrame) ?? selections[0];
         const from = dayjs(selection.from).format('YYYY-MM-DD');
         const to = dayjs(selection.to).format('YYYY-MM-DD');
         getStationStatistics(props.station.id.toString(), from, to).then((d) => {
