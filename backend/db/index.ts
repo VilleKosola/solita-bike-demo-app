7eb5dc3250040f4b6be05a8557374d34690ea1d9
Ville Kosola <ville.kosola@solita.fi>
2022-11-23 11:48:36 +0200

    add db function for copy

--- backend/db/index.ts
+++ backend/db/index.ts
@@ -1,4 +1,6 @@
-import { Pool, QueryResult } from 'pg';
+import { Pool } from 'pg';
+import {from as copyFrom} from 'pg-copy-streams'
+import { Readable } from 'stream';
 
   const client: Pool = new Pool({
     user: 'bikeapp',
@@ -12,3 +14,22 @@ import { Pool, QueryResult } from 'pg';
     export const query = (text: string, params?: any[]) => {
       return client.query(text, params)
     }
+
+
+    export function queryWithCopy(query: string, csv: string) {
+      client.connect(function (err, client, done) {
+        const stream = client.query(copyFrom(query))
+        const s = new Readable();
+        s._read = () => {}; // redundant? see update below
+        s.push(csv);
+        s.push(null);
+        stream.on('error', done)
+        stream.on('finish', (e) => {
+          console.timeEnd('journeys to db');         
+          console.timeEnd('ImportData Process')
+          console.log('Data import to DB successful!'); 
+          done(e);})
+        s.pipe(stream)
+      })
+    }
+
