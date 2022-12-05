c3d54c9760260b808fccb2d06eb1ac1143be2a31
Ville Kosola <ville.kosola@solita.fi>
2022-12-05 15:21:14 +0200

    add hover effect

--- frontend/src/components/navitem/index.tsx
+++ frontend/src/components/navitem/index.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames';
 import { Link } from 'react-router-dom';
 
 interface NavItemProps {
@@ -10,9 +11,10 @@ const NavItem = (props: NavItemProps) => {
   return (
     <Link
       to={props.path}
-      className={
-        props.active ? 'p-3 bg-slate-600 text-white' : 'p-3 bg-slate-200'
-      }
+      className={classNames(
+        props.active ? 'p-3 bg-slate-600 text-white' : 'p-3 bg-slate-200',
+        'hover:bg-slate-300'
+      )}
     >
       {props.name}
     </Link>
