f6dc7e3409a85c858cf3a7ffc1ec432bc90efec5
Ville Kosola <ville.kosola@solita.fi>
2022-12-05 15:22:03 +0200

    finalize journey add

--- frontend/src/components/add-journey/index.tsx
+++ frontend/src/components/add-journey/index.tsx
@@ -5,24 +5,36 @@ import { Journey } from '../../types/journey';
 import InputWithLabel from '../input-with-label';
 import MapWrapper from '../map-wrapper';
 import Searchbar from '../searchbar';
+import DateTimePicker from 'react-datetime-picker';
+import dayjs from 'dayjs';
 
 const defaultJourney = {
   return_station: 1,
   return_station_name: 'Kaivopuisto',
   departure_station: 2,
   departure_station_name: 'Laivasillankatu',
-  departuredate: '',
+  departuredate: new Date(),
   returndate: '',
   distance: 10,
   duration: 10,
-} as Journey;
+};
 
-const AddJourney = () => {
+const AddJourney = (props: {
+  active: boolean;
+  handleActivation: { (value: string): void };
+}) => {
   const [newJourney, setNewJourney] = useState(defaultJourney);
   const stations = useStations();
 
   const sendStation = () => {
-    postJourney(newJourney);
+    postJourney({
+      ...newJourney,
+      departuredate: getFormattedDate(newJourney.departuredate, 0),
+      returndate: getFormattedDate(
+        newJourney.departuredate,
+        newJourney.duration
+      ),
+    } as Journey);
     setNewJourney(defaultJourney);
   };
 
@@ -38,53 +50,74 @@ const AddJourney = () => {
       [name]: value,
     });
   };
+  const handleStartDateChange = (dateTime: Date) => {
+    setNewJourney({
+      ...newJourney,
+      departuredate: dateTime,
+    });
+  };
+
+  const getFormattedDate = (date: Date, offset: number) => {
+    return dayjs(date).add(offset, 's').format('YYYY-MM-DDTHH:mm:ss[Z]');
+  };
 
   return (
     <>
-      <div className="w-full text-center font-bold text-lg border-t-2 border-t-gray-300">
+      <div
+        className="w-full text-center font-bold text-lg border-t-2 border-t-gray-300 mt-2 hover:bg-slate-200 cursor-pointer"
+        onClick={() => props.handleActivation('J')}
+      >
         Journey
       </div>
-      <Searchbar
-        searchStringChange={() => ''}
-        searchString={newJourney.departure_station_name.toString()}
-        stations={stations}
-        name="departure_station"
-        stationIdChange={handleStationIdChange}
-      ></Searchbar>
-      <Searchbar
-        searchStringChange={() => ''}
-        searchString={newJourney.return_station_name.toString()}
-        stations={stations}
-        name="return_station"
-        stationIdChange={handleStationIdChange}
-      ></Searchbar>
-      <InputWithLabel
-        label={'Distance'}
-        type={'number'}
-        value={newJourney.distance}
-        changeFn={handleChange}
-        name={'distance'}
-      ></InputWithLabel>
-      <InputWithLabel
-        label={'Duration'}
-        type={'number'}
-        value={newJourney.duration}
-        changeFn={handleChange}
-        name={'duration'}
-      ></InputWithLabel>
-      <button
-        className="border border-gray-500 border-solid p-1 m-2"
-        onClick={sendStation}
-      >
-        Add New
-      </button>
+      {props.active && (
+        <>
+          <Searchbar
+            searchStringChange={() => ''}
+            searchString={newJourney.departure_station_name.toString()}
+            stations={stations}
+            name="departure_station"
+            stationIdChange={handleStationIdChange}
+          ></Searchbar>
+          <Searchbar
+            searchStringChange={() => ''}
+            searchString={newJourney.return_station_name.toString()}
+            stations={stations}
+            name="return_station"
+            stationIdChange={handleStationIdChange}
+          ></Searchbar>
+          <InputWithLabel
+            label={'Distance'}
+            type={'number'}
+            value={newJourney.distance}
+            changeFn={handleChange}
+            name={'distance'}
+          ></InputWithLabel>
+          <DateTimePicker
+            onChange={handleStartDateChange}
+            value={new Date(newJourney.departuredate)}
+          ></DateTimePicker>
+          <InputWithLabel
+            label={'Duration'}
+            type={'number'}
+            value={newJourney.duration}
+            changeFn={handleChange}
+            name={'duration'}
+          ></InputWithLabel>
+          <button
+            className="border border-gray-500 border-solid p-1 m-2"
+            onClick={sendStation}
+          >
+            Add New
+          </button>
 
-      <MapWrapper
-        stationIds={[
-          newJourney.departure_station.toString(),
-          newJourney.return_station.toString(),
-        ]}
-      ></MapWrapper>
+          <MapWrapper
+            stationIds={[
+              newJourney.departure_station.toString(),
+              newJourney.return_station.toString(),
+            ]}
+          ></MapWrapper>
+        </>
+      )}
     </>
   );
 };
